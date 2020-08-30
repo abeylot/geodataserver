63db4bb6f02e720b124f4a16ac0597fbffe1a931
A. Beylot <alain.beylot@free.fr>
2020-08-30 10:39:54 +0200

    add map display service

---
+++ services/MapDisplay.cpp
@@ -0,0 +1,19 @@
+#include "MapDisplay.hpp"
+
+MapDisplay::MapDisplay()
+{
+}
+
+MapDisplay::~MapDisplay()
+{
+}
+
+Msg* MapDisplay::processRequest(Msg* request, CompiledDataManager& mger)
+{
+    std::string resp = "<!DOCTYPE html><html> <head>  <meta charset=\"UTF-8\"></head> <body>Ping!</body></html>";
+    Msg* rep = new Msg;
+    encoder.build200Header(rep, "text/html");
+    encoder.addContent(rep,resp);
+    return rep;
+}
+
---
+++ services/MapDisplay.hpp
@@ -0,0 +1,13 @@
+#ifndef MAPDISPLAY_HPP
+#define MAPDISPLAY_HPP
+
+
+class MapDisplay : public ServiceInterface
+{
+public:
+    MapDisplay();
+    ~MapDisplay();
+
+};
+
+#endif // MAPDISPLAY_HPP
