1baf0d2b087130a5a29c8ea94cfccd69b3967557
abeylot <vous@exemple.com>
2024-02-13 09:14:01 +0100

    thread sanitization

--- helpers/ExtThread.hpp
+++ helpers/ExtThread.hpp
@@ -5,46 +5,46 @@
 
 namespace ExtThreads
 {
-	bool shall_stop;
-	
-	std::vector<std::thread*> ext_threads;
-	
-	
-	void init()
-	{
-		shall_stop = false;
-	}
-	
-	template<class F>
-	void launch_thread(F f)
-	{
-		std::thread* t = new std::thread(f);
-		ext_threads.push_back(t);
-	}
-	
-	void request_all_to_stop()
-	{
-	    shall_stop = true;
-	}
+    std::atomic<bool> shall_stop;
 
-	void request_all_to_join()
-	{
-		for(auto &t : ext_threads)
-		{
-			t->join();
-		} 
+    std::vector<std::thread*> ext_threads;
 
-		for(auto &t : ext_threads)
-		{
-			delete(t);
-		}
-		ext_threads.clear(); 
-	}
-	
-	bool stop_requested()
-	{
-		return shall_stop;
-	}
-	
+
+    void init()
+    {
+        shall_stop = false;
+    }
+
+    template<class F>
+    void launch_thread(F f)
+    {
+        std::thread* t = new std::thread(f);
+        ext_threads.push_back(t);
+    }
+
+    void request_all_to_stop()
+    {
+        shall_stop = true;
+    }
+
+    void request_all_to_join()
+    {
+        for(auto &t : ext_threads)
+        {
+            t->join();
+        }
+
+        for(auto &t : ext_threads)
+        {
+            delete(t);
+        }
+        ext_threads.clear();
+    }
+
+    bool stop_requested()
+    {
+        return shall_stop;
+    }
+
 }
 #endif
--- helpers/NonGrowableQueue.hpp
+++ helpers/NonGrowableQueue.hpp
@@ -5,65 +5,67 @@
 template<class T, int SIZE>
 class NonGrowableQueue
 {
-	private:
-	
-	std::mutex read_token;
-	std::mutex write_token;
-	
-	int next_in_key;
-	int next_out_key;
-	T items[SIZE];
-	bool itemIsUsed[SIZE];
-	
-	
-	public:
-	
-	NonGrowableQueue():next_in_key(0),next_out_key(0)
-	{
-		for(int i = 0; i < SIZE; i++) itemIsUsed[i] = false;
-	}
+    private:
+
+    std::mutex _token;
+
+    int next_in_key;
+    int next_out_key;
+    T items[SIZE];
+    bool itemIsUsed[SIZE];
 
 
-	
-	bool empty()
-	{
-		return(!itemIsUsed[next_out_key]);
-	}
-	
-	bool push(T item)
-	{
+    public:
 
-        write_token.lock();
-		
-		int key = next_in_key; 
-		if(itemIsUsed[key])
-		{
-			write_token.unlock();			
-			return false;
-		} else {
-			next_in_key = ( next_in_key + 1 ) % SIZE;;
-			items[key] = item;
-			itemIsUsed[key] = true;
-			write_token.unlock();			
-			return true;	
-		}
-	}
-	
-	bool pop(T& item)
-	{
-        read_token.lock();
-		int key = next_out_key;
-		if(!itemIsUsed[key])
-		{
-			read_token.unlock();
-			return false;
-		} else {
-			item = items[key];
-			itemIsUsed[key] = false;
-			next_out_key = (next_out_key + 1) % SIZE;
-			read_token.unlock();
-			return true;	
-		}
-	}
-	
+    NonGrowableQueue():next_in_key(0),next_out_key(0)
+    {
+        for(int i = 0; i < SIZE; i++) itemIsUsed[i] = false;
+    }
+
+
+
+    bool empty()
+    {
+        _token.lock();
+        bool res = !itemIsUsed[next_out_key];
+        _token.unlock();
+        return res;
+    }
+
+    bool push(T item)
+    {
+
+        _token.lock();
+
+        int key = next_in_key;
+        if(itemIsUsed[key])
+        {
+            _token.unlock();
+            return false;
+        } else {
+            next_in_key = ( next_in_key + 1 ) % SIZE;;
+            items[key] = item;
+            itemIsUsed[key] = true;
+            _token.unlock();
+            return true;
+        }
+    }
+
+    bool pop(T& item)
+    {
+        _token.lock();
+        int key = next_out_key;
+        if(!itemIsUsed[key])
+        {
+            _token.unlock();
+            return false;
+        } else {
+            item = items[key];
+            itemIsUsed[key] = false;
+            next_out_key = (next_out_key + 1) % SIZE;
+            _token.unlock();
+            return true;
+        }
+    }
+
 };
